cba4abd99e71162b130cd12f6d19d2639e351da3
christric1 <ra2847545015@gmail.com>
2021-06-04 15:11:39 +0800

    add new file

---
+++ Ricochet Robots/Board.h
@@ -0,0 +1,240 @@
+﻿#include <vector>
+#include <cstdlib> /* 亂數相關函數 */
+#include <ctime>   /* 時間相關函數 */
+#include <random>
+#include <math.h>
+
+using namespace std;
+
+enum Attribute {Origin, End, Null};   // 起點、終點
+enum Color {Red, Blue, Yellow, Green, All, None};  // 顏色
+enum Direction {Right, Left, Up, Dowm}; // 移動方位
+
+random_device rd;
+default_random_engine gen = std::default_random_engine(rd());
+uniform_int_distribution<int> dis(1, );
+
+static const char* enum_Direction[] =
+{ "Right", "Left", "Up", "Dowm" };
+
+static const char* enum_Color[] =
+{ "Red", "Blue", "Yellow", "Green", "All", "None" };
+
+Color DetermineColor(int c) {  // 決定顏色
+	if (c == 2 || c == 7 || c == 10 || c == 16 || c == 20)
+		return Red;
+	else if (c == 3 || c == 13 || c == 15 || c == 18 || c == 21)
+		return Yellow;
+	else if (c == 4 || c == 9 || c == 11 || c == 14 || c == 19)
+		return Green;
+	else if (c == 5 || c == 6 || c == 8 || c == 12 || c == 17)
+		return Blue;
+	else if (c == 1)  // 彩色終點
+		return All;
+
+}
+
+void Change(char* buffer, int* trob) {
+	if (buffer[1] == ' ') {
+		trob[0] = buffer[0] - 48;
+		for (int i = 1; i < 5; i++) {
+			trob[i] = buffer[i + 1] - 48;
+		}
+	}
+
+	else {
+		int temp = (buffer[0] - 48) * 10 + buffer[1] - 48;
+		trob[0] = temp;
+		for (int i = 1; i < 5; i++) {
+			trob[i] = buffer[i + 2] - 48;
+		}
+	}
+}
+
+class Grid 
+{
+public:
+
+	Attribute at;
+	Color cr;
+	int Row = 0;
+	int Column = 0;
+	bool barrier[4] = { false };  // 上、右、下、左
+
+	Grid(Attribute  at = Null, Color co = None) : at(at), cr(co){}  // 建構子
+
+	void SetAttribute(Attribute a) {
+		at = a;
+	}
+
+	void SetColor(Color c) {
+		cr = c;
+	}
+};
+
+class Board
+{
+public:
+	vector <Grid>  Bo[256];  // 棋盤
+
+	vector <Grid> OriginPointer;  // 指向起點指標 -- 紅 綠 藍 黃
+	vector <Grid> PresentPosition;  // 指向現在位置的指標
+	vector <Grid> RedEndPointer;  // 指向紅色終點指標
+	vector <Grid> GreenEndPointer;  // 指向綠色終點指標
+	vector <Grid> BlueEndPointer;  //指向藍色終點指標
+	vector <Grid> YellowEndPointer;  //指向黃色終點指標
+	Grid* MulticolorEndPointer;  // 彩色終點指標
+
+	void AddBoard(Grid &gr, int i) {  // 放入棋盤
+		Bo[i].push_back(gr);
+	}
+
+	void SetOriginPointer( Grid &gr) {  
+		OriginPointer.push_back(gr);
+	}
+	
+	vector<Grid> GetEndPointer(Color co) {  // 回傳顏色終點vector
+		if (co == Red)
+			return RedEndPointer;
+		else if (co == Green)
+			return GreenEndPointer;
+		else if (co == Blue)
+			return BlueEndPointer;
+		else if (co == Yellow)
+			return YellowEndPointer;
+	}
+
+	void SetEndPointer(Color co, Grid &gr) {
+		if (co == Red)
+			RedEndPointer.push_back(gr);
+		else if (co == Green)
+			GreenEndPointer.push_back(gr);
+		else if (co == Blue)
+			BlueEndPointer.push_back(gr);
+		else if (co == Yellow)
+			YellowEndPointer.push_back(gr);
+		else 
+			MulticolorEndPointer = &gr;   // 彩色終點
+	}
+
+	Direction GetDirection(Grid PresentLocation) {    // 得到方向
+
+		// cout << PresentLocation.Row << " " << PresentLocation.Column << endl;
+
+		Direction DirectionQueue[4] = { Right, Left, Up, Dowm };  // 優先次序
+
+		int a = dis(gen) % 4;
+		while(CheckHaveBlock(PresentLocation, DirectionQueue[a]))      // 看方向是否立刻有障礙物   
+			a = dis(gen) % 4;
+		return DirectionQueue[a];
+	}
+
+	bool FourCheck(Grid ori) {
+
+		Grid temp;
+		for (int i = 0; i < PresentPosition.size(); i++) {
+			temp = PresentPosition.at(i);
+			if (ori.Row == temp.Row && ori.Column == temp.Column)
+				return true;
+		}
+		return false;
+	}
+
+	bool CheckHitOtherPiece(Grid gr, Direction di) {  // 確認是否撞到其他棋子
+
+		switch (di){
+
+		case Right:
+			if (FourCheck(Bo[gr.Row].at(gr.Column+1)))
+				return true;
+			break;
+
+		case Left:
+			if (FourCheck(Bo[gr.Row].at(gr.Column - 1)))
+				return true;
+			break;
+
+		case Up:
+			if (FourCheck(Bo[gr.Row-1].at(gr.Column)))
+				return true;
+			break;
+
+		case Dowm:
+			if (FourCheck(Bo[gr.Row+1].at(gr.Column)))
+				return true;
+			break;
+		}
+
+		return false;
+	}
+
+	bool CheckHaveBlock(Grid gr, Direction di) {  // 確認移動的方向是否有牆壁
+
+		if (di == Right) 
+			if (gr.barrier[1] || CheckHitOtherPiece(gr, di)) 
+				return true;
+		
+		if (di == Dowm)
+			if (gr.barrier[2] || CheckHitOtherPiece(gr, di))
+				return true;
+
+		if (di == Left)
+			if (gr.barrier[3] || CheckHitOtherPiece(gr, di))
+				return true;
+
+		if (di == Up)
+			if (gr.barrier[0] || CheckHitOtherPiece(gr, di))
+				return true;
+		
+		return false;
+	}
+
+	Grid KeepGoingBro(Direction di, Grid PresentLocation, Color co) {  // 持續前進
+		while (!ArriveEndCheck(PresentLocation, co) && !CheckHaveBlock(PresentLocation, di)) {  // 到終點 or 撞到牆壁 or 撞到其他棋子
+			PresentLocation = MoveOneStep(di, PresentLocation);  // 移動一步
+			// cout << PresentLocation.Row << " " << PresentLocation.Column << endl;
+		}
+		return PresentLocation;
+	}
+
+	Grid MoveOneStep(Direction di, Grid PresentLocation) {  // 移動一步
+		switch (di)
+		{
+		case Right:
+			PresentLocation = Bo[PresentLocation.Row].at(PresentLocation.Column + 1);
+			return PresentLocation;
+		case Left:
+			PresentLocation = Bo[PresentLocation.Row].at(PresentLocation.Column - 1);
+			return PresentLocation;
+		case Up:
+			PresentLocation = Bo[PresentLocation.Row - 1].at(PresentLocation.Column);
+			return PresentLocation;
+		case Dowm:
+			PresentLocation = Bo[PresentLocation.Row + 1].at(PresentLocation.Column);
+			return PresentLocation;
+		default:
+			return PresentLocation;
+		}
+	}
+
+	bool ArriveEndCheck(Grid Origin, Color co) {  // 確認是否到達終點
+
+		vector<Grid> temp = GetEndPointer(co);
+
+		if (Origin.Row == MulticolorEndPointer->Row && Origin.Column == MulticolorEndPointer->Column)
+			return true;
+
+		for (int i = 0; i < temp.size(); i++) {
+			if (Origin.Row == temp.at(i).Row && Origin.Column == temp.at(i).Column)
+				return true;
+		}
+		return false;
+	}
+
+	void ClearAll() {   // 初始化
+		Color co = Bo[7].at(7).cr;  // 中間終點顏色
+		for (int i = 0; i < 4; i++)    // 把中間終點拔除
+			GetEndPointer(co).pop_back();  
+		PresentPosition.clear();  // 目前棋子位置全部清除
+	}
+};
---
+++ Ricochet Robots/main2.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include "Board.h"
+
+using namespace std;
+
+int main(void) {
+
+	FILE* pFile;
+    char buffer[10];   // 足夠容納一行字的 buffer
+    int Trob[5];
+	errno_t err;  
+	err = fopen_s(&pFile, "board.txt", "r");   // 檔案打開成功返回 0 ，失敗返回 1
+
+    if (err != 0)  
+    { 
+        perror("檔案不存在");
+    }
+
+    else
+    {
+        Grid gr[256]; // 建立 256 格子
+        int i = 0;
+        cout << "開檔成功" << endl;
+
+        Board bod;  // 建立棋盤
+
+        while (fgets(buffer, 10, pFile))  // 用 fgets 一次讀進一行
+        {
+            Change(buffer, Trob);
+
+            gr[i].Row = i / 16;
+            gr[i].Column = i % 16;
+
+            if (Trob[1] == 1)     // 牆壁
+                gr[i].barrier[0] = true;
+            if (Trob[2] == 1)
+                gr[i].barrier[1] = true;
+            if (Trob[3] == 1)
+                gr[i].barrier[2] = true;
+            if (Trob[4] == 1)
+                gr[i].barrier[3] = true;
+            
+            if (Trob[0] == 6 || Trob[0] == 7 || Trob[0] == 14 || Trob[0] == 21) {   // 起點
+                gr[i].SetAttribute(Origin);
+                gr[i].SetColor(DetermineColor(Trob[0]));
+
+                bod.SetOriginPointer(gr[i]);  // 設定起點指標
+            }
+
+            else if (Trob[0] != 0) {   // 終點
+                gr[i].SetAttribute(End);
+                gr[i].SetColor(DetermineColor(Trob[0]));
+
+                bod.SetEndPointer(DetermineColor(Trob[0]), gr[i]);  // 設定終點指標
+            }
+
+            bod.AddBoard(gr[i], i/16);  // 加入棋盤
+
+            i++;
+        }
+
+        cout << "初始盤面已讀取完畢...請開始遊戲" << endl << endl;
+
+        for (int j = 1; j <= 5; j++) {  // 五回合
+            cout << "第" << j << "回合" << endl;
+            cout << "請輸入終點 : ";
+           
+            int EndPoint;
+            cin >> EndPoint;
+            cout << endl;
+
+            int arr[4] = { 119, 120, 135, 136 };
+            for (int i = 0; i < 4; i++) {     // 設定中央終點
+                gr[arr[i]].SetAttribute(End);
+                gr[arr[i]].SetColor(DetermineColor(EndPoint));
+                bod.Bo[arr[i] / 16].at(arr[i] % 16) = gr[arr[i]];
+                bod.SetEndPointer(DetermineColor(EndPoint), gr[arr[i]]);  // 設定終點指標
+            }
+
+            int TotalStep = 0;
+            cout << "Path : " << endl;
+
+            for (int i = 0; i < bod.OriginPointer.size(); i++)     // 棋子位置指標設定
+                bod.PresentPosition.push_back(bod.OriginPointer.at(i));
+
+            for (int i = 0; i < bod.OriginPointer.size(); i++) {   // 開始起點巡迴
+
+                Direction di;  // 方位
+                Grid OriginGrid;  // 起點
+                Grid PresentLocation;  // 目前位置
+
+                OriginGrid = bod.OriginPointer.at(i);
+                PresentLocation = OriginGrid;
+
+                while (!bod.ArriveEndCheck(PresentLocation, OriginGrid.cr)) {
+                    di = bod.GetDirection(PresentLocation);  // 得移動到方向
+                    cout << enum_Color[OriginGrid.cr] << "  " << enum_Direction[di] << endl;
+
+                    PresentLocation = bod.KeepGoingBro(di, PresentLocation, OriginGrid.cr);  //按照方位前進
+                    bod.PresentPosition.at(i) = PresentLocation;
+                    TotalStep++;
+                }
+            }
+            cout << endl << "Total Step : " << TotalStep << endl << endl;
+            cout << "------------------------------------------" << endl << endl;
+        }
+
+        bod.ClearAll();  // 初始化 &&　歸零
+    }
+       
+    fclose(pFile);   // 關閉檔案
+    return 0;
+}
